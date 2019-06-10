1207c338b9549a9c8110604e35953f50a67cb3d3
Serhii.Shramko <shramko.web@yahoo.com>
2019-06-10 08:48:14 +0300

    базова структура и реализация ф-и получния случ. итема в масиве

---
+++ js/setup.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var WIZZARD_SETUP = document.querySelector('.setup');
+var WIZZARDS_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARDS_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var WIZZARDS_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(56, 159, 117', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var WIZARDS_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var removeHiddenClass = function (element) {
+  element.classList.remove('hidden');
+};
+
+removeHiddenClass(WIZZARD_SETUP);
+
+var getRandomItemFrom = function (array) { // получаем случайный элемент в переданом масиве
+  var arrayLength = array.length;
+  var randomItem = array[Math.floor(Math.random() * array.length)];
+  return randomItem;
+}
