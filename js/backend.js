15789c7620efb878b6bfdf6916269f6bda8574db
Serhii.Shramko <shramko.web@yahoo.com>
2019-06-24 13:46:28 +0300

    оптимизация модуля backend & добавил новый метод в util

--- js/backend.js
+++ js/backend.js
@@ -3,54 +3,10 @@
 window.backend = (function () {
   return {
     load: function (onLoad, onError) {
-      var URL = 'https://js.dump.academy/code-and-magick/data';
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
-
-      xhr.addEventListener('load', function () {
-        if (xhr.status === 200) {
-          onLoad(xhr.response);
-        } else {
-          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-        }
-      });
-      xhr.addEventListener('error', function () {
-        onError('Произошла ошибка соединения (сервер не доступен)');
-      });
-      xhr.addEventListener('timeout', function () {
-        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-      });
-
-      xhr.timeout = 10000; // 10s
-
-      xhr.open('GET', URL);
-      xhr.send();
+      window.util.xhr('https://js.dump.academy/code-and-magick/data', 'GET', onLoad, onError);
     },
     save: function (data, onLoad, onError) {
-      var URL = 'https://js.dump.academy/code-and-magick';
-
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
-
-      xhr.addEventListener('load', function () {
-        if (xhr.status === 200) {
-          onLoad(xhr.response);
-        } else {
-          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-        }
-      });
-
-      xhr.addEventListener('error', function () {
-        onError('Произошла ошибка отправки формы');
-      });
-      xhr.addEventListener('timeout', function () {
-        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-      });
-
-      xhr.timeout = 10000; // 10s
-
-      xhr.open('POST', URL);
-      xhr.send(data);
+      window.util.xhr('https://js.dump.academy/code-and-magick', 'POST', onLoad, onError, data);
     }
   };
 })();
--- js/util.js
+++ js/util.js
@@ -14,6 +14,36 @@ window.util = (function () {
       if (evt.keyCode === ENTER_KEYCODE) {
         action();
       }
+    },
+    xhr: function (url, method, onSuccess, onError, data) {
+      var xhr = new XMLHttpRequest();
+
+      xhr.responseType = 'json';
+
+      xhr.addEventListener('load', function () {
+        if (xhr.status === 200) {
+          onSuccess(xhr.response);
+        } else {
+          onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        }
+      });
+
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка соединения');
+      });
+
+      xhr.addEventListener('timeout', function () {
+        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+      });
+
+      xhr.timeout = 10000; // 10s
+
+      xhr.open(method, url);
+      if (data) {
+        xhr.send(data);
+      } else {
+        xhr.send();
+      }
     }
   };
 })();
