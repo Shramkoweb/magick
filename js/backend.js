5304678054bada6d90d8958abf7c83f472eef040
Serhii.Shramko <shramko.web@yahoo.com>
2019-06-25 12:06:29 +0300

    перенес модуль запроса в backend & фикс урла формы

--- js/backend.js
+++ js/backend.js
@@ -1,12 +1,44 @@
 'use strict';
 
 window.backend = (function () {
+
+  var request = function (url, method, onSuccess, onError, data) {
+    var xhrRequest = new XMLHttpRequest();
+
+    xhrRequest.responseType = 'json';
+
+    xhrRequest.addEventListener('load', function () {
+      if (xhrRequest.status === 200) {
+        onSuccess(xhrRequest.response);
+      } else {
+        onError('Cтатус ответа: ' + xhrRequest.status + ' ' + xhrRequest.statusText);
+      }
+    });
+
+    xhrRequest.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhrRequest.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhrRequest.timeout + 'мс');
+    });
+
+    xhrRequest.timeout = 10000; // 10s
+
+    xhrRequest.open(method, url);
+    if (data) {
+      xhrRequest.send(data);
+    } else {
+      xhrRequest.send();
+    }
+  };
+
   return {
     load: function (onLoad, onError) {
-      window.request('https://js.dump.academy/code-and-magick/data', 'GET', onLoad, onError);
+      request('https://js.dump.academy/code-and-magick/data', 'GET', onLoad, onError);
     },
     save: function (data, onLoad, onError) {
-      window.request('https://js.dump.aademy/code-and-magick', 'POST', onLoad, onError, data);
+      request('https://js.dump.academy/code-and-magick', 'POST', onLoad, onError, data);
     }
   };
 })();
